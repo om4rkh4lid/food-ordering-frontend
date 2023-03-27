0771b082da04021670c1217cf00c020ca5f64e66
Omar Khalid <xom4rxkh4lidx@gmail.com>
2023-03-27 23:41:47 +0200

    added search button

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './NavBar.css';
 
 interface NavBarProps {
@@ -11,6 +12,7 @@ const NavBar: React.FC<NavBarProps> = (props: NavBarProps) => {
       <img className="app-logo" src="https://blog.talabat.com/wp-content/uploads/2020/07/cropped-Talabat-New-Brand-Icon-2.png" />
       <div className="search-group">
         <input type="search" name="search" placeholder="Search here..." />
+        <button type="submit">Search</button>
       </div>
       <div className="profile-pic">
         <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
