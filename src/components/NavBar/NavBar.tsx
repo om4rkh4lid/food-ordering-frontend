be3871f93b296956ce10d4beae037d54e0c97048
Omar Khalid <xom4rxkh4lidx@gmail.com>
2023-03-31 22:22:21 +0200

    navbar handles search queries correctly

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,20 +1,34 @@
-import { HTMLInputEvent } from '../../types';
+import { useState } from 'react';
+import { ChangeEvent, SubmitEvent } from '../../types';
+import CompanyLogo from '../../assets/talabat_logo.png';
 import './NavBar.css';
 
 interface NavBarProps {
   appLogoUrl: string;
   profilePicUrl: string;
-  searchCallback: (event: HTMLInputEvent) => void;
+  searchCallback: (query: string) => void;
 }
 
 const NavBar: React.FC<NavBarProps> = (props: NavBarProps) => {
+
+  const [searchQuery, setSearchQuery] = useState('');
+  
+  const handleInputChange = (event: ChangeEvent) => {
+    setSearchQuery(event.target.value);
+  }
+
+  const search = (event: SubmitEvent) =>  {
+    event.preventDefault();
+    props.searchCallback(searchQuery);
+  }
+
   return (
     <nav className="navbar">
-      <img className="app-logo" src="https://blog.talabat.com/wp-content/uploads/2020/07/cropped-Talabat-New-Brand-Icon-2.png" />
-      <div className="search-group">
-        <input type="search" name="search" placeholder="Search here..." />
+      <img className="app-logo" src={CompanyLogo} />
+      <form className="search-group" onSubmit={search}>
+        <input type="search" name="search" placeholder="Search here..." value={searchQuery} onChange={handleInputChange}/>
         <button type="submit">Search</button>
-      </div>
+      </form>
       <div className="profile-pic">
         <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
       </div>
