93c78892ab50cdfbbc6de0856ce918fe45e9fc94
Omar Khalid <xom4rxkh4lidx@gmail.com>
2023-03-31 17:19:18 +0200

    created callback function to lift the state up from the navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,15 @@ import companyLogo from './assets/talabat_logo.png';
 import profilePicture from './assets/profile_pic.png';
 import NavBar from './components/NavBar/NavBar'
 import ClientContent from './pages/ClientContent/ClientContent';
+import { useState } from 'react';
 
 function App() {
+
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value)
+  }
 
   return (
     <>
