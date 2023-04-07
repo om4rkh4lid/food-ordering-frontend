85c4d9b73023eeda10520f3f162c085b9a4ea97b
Omar Khalid <xom4rxkh4lidx@gmail.com>
2023-04-07 22:15:40 +0200

    changed class names for the cart item component to avoid clashing with menu item

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -3,21 +3,21 @@ import companyLogo from "../../assets/talabat_logo.png";
 
 export const CartItem: React.FC = () => {
   return (
-    <div className="cart-item">
-      <div className="item-image">
+    <div className="ci-cart-item">
+      <div className="ci-item-image">
         <img src={companyLogo} />
       </div>
-      <div className="item-info">
-        <p className="item-name">Name</p>
-        <div className='cart-ctrl-group'>
-          <button className='cart-ctrl-btn' onClick={() => { }}>+</button>
-          <span className='cart-qty'>x2</span>
-          <button className='cart-ctrl-btn' onClick={() => { }}>-</button>
+      <div className="ci-item-info">
+        <p className="ci-item-name">Name</p>
+        <div className='ci-cart-ctrl-group'>
+          <button className='ci-cart-ctrl-btn' onClick={() => { }}>+</button>
+          <span className='ci-cart-qty'>x2</span>
+          <button className='ci-cart-ctrl-btn' onClick={() => { }}>-</button>
         </div>
       </div>
-      <div className="action-section">
-        <div className="price"><p>$100</p></div>
-        <button className="remove">X</button>
+      <div className="ci-action-section">
+        <div className="ci-price"><p>$100</p></div>
+        <button className="ci-remove">X</button>
       </div>
     </div>
   );
