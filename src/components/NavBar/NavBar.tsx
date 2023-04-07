606765ba77249a60a5f3ff758473d4593385d4c5
Omar Khalid <xom4rxkh4lidx@gmail.com>
2023-04-07 15:19:30 +0200

    added a cart button to the navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { ChangeEvent, SubmitEvent } from '../../types';
-import CompanyLogo from '../../assets/talabat_logo.png';
+import companyLogo from '../../assets/talabat_logo.png';
+import profilePicture from '../../assets/profile_pic.png';
 import './NavBar.css';
 import { useNavigate } from 'react-router-dom';
 
@@ -13,25 +14,36 @@ const NavBar: React.FC<NavBarProps> = (props: NavBarProps) => {
 
   const navigate = useNavigate();
   const [searchQuery, setSearchQuery] = useState('');
-  
+
   const handleInputChange = (event: ChangeEvent) => {
     setSearchQuery(event.target.value);
   }
 
-  const search = (event: SubmitEvent) =>  {
+  const search = (event: SubmitEvent) => {
     event.preventDefault();
     navigate(`/restaurants?name=${searchQuery}`);
   }
 
   return (
     <nav className="navbar">
-      <img className="app-logo" src={CompanyLogo} />
+      <img className="app-logo" src={companyLogo} />
       <form className="search-group" onSubmit={search}>
-        <input type="search" name="search" placeholder="Search here..." value={searchQuery} onChange={handleInputChange}/>
+        <input type="search" name="search" placeholder="Search here..." value={searchQuery} onChange={handleInputChange} />
         <button type="submit">Search</button>
       </form>
-      <div className="profile-pic">
-        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
+      <div className="navbar-controls">
+        <div className="profile-pic">
+          <img src={profilePicture} />
+        </div>
+        <button className="cart">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 576 512"
+            fill="currentColor"
+          >
+            <path d="M96 0C107.5 0 117.4 8.19 119.6 19.51L121.1 32H541.8C562.1 32 578.3 52.25 572.6 72.66L518.6 264.7C514.7 278.5 502.1 288 487.8 288H170.7L179.9 336H488C501.3 336 512 346.7 512 360C512 373.3 501.3 384 488 384H159.1C148.5 384 138.6 375.8 136.4 364.5L76.14 48H24C10.75 48 0 37.25 0 24C0 10.75 10.75 0 24 0H96zM128 464C128 437.5 149.5 416 176 416C202.5 416 224 437.5 224 464C224 490.5 202.5 512 176 512C149.5 512 128 490.5 128 464zM512 464C512 490.5 490.5 512 464 512C437.5 512 416 490.5 416 464C416 437.5 437.5 416 464 416C490.5 416 512 437.5 512 464z" />
+          </svg>
+        </button>
       </div>
     </nav>
   );
