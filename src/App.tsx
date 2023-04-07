b014034668838a9465ea26becf8f62bfc0647aac
Omar Khalid <xom4rxkh4lidx@gmail.com>
2023-04-07 15:55:00 +0200

    added the cart and not found pages to the router

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './pages/Client/Home/Home';
 import Restaurants from './pages/Client/Restaurants/Restaurants';
 import Menu from './pages/Client/Menu/Menu';
+import { Cart } from './pages/Client/Cart/Cart';
+import { NotFound } from './pages/NotFound/NotFound';
 
 
 
@@ -14,8 +16,10 @@ function App() {
       <Routes>
         <Route path='/' element={<ClientContent />}>
           <Route path='' element={<Home/>} />
+          <Route path='cart' element={<Cart />} />
           <Route path='restaurants' element={<Restaurants />} />
           <Route path='restaurants/:id' element={<Menu />} />
+          <Route path='*' element={<NotFound />} />
         </Route>
       </Routes>
     </Router>
