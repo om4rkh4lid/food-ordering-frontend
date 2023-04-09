e1ed41ce95a6c3e9f9d2b90a9604a31996c91837
Omar Khalid <xom4rxkh4lidx@gmail.com>
2023-04-09 07:04:47 +0200

    lifted logic of fetching cart items up to the cart page to calculate total

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -1,54 +1,29 @@
 import "./CartItem.css";
-import companyLogo from "../../assets/talabat_logo.png";
-import { CartItem as CartItemType } from "../../contexts/CartContext";
-import { useEffect, useState } from "react";
-import axiosInstance from "../../api/axios";
 import MenuItem from "../../entities/MenuItem";
 import { useCart } from "../../hooks/useCart";
 type CartItemProps = {
-  item: CartItemType;
+  item: MenuItem;
 }
 
 export const CartItem: React.FC<CartItemProps> = ({ item }) => {
-
-  const [menuItem, setMenuItem] = useState({} as MenuItem);
-  const { removeItem, incrementItem, decrementItem } = useCart();
-
-  useEffect(() => {
-    const query = `{
-      menuItem(itemId: ${item.id}){
-        id
-        name
-        price
-        photo
-        description
-      }
-    }`
-    axiosInstance.post('/', { query })
-      .then(result => {
-        const item = result.data.menuItem;
-        setMenuItem(new MenuItem(item.id, item.name, item.price, item.description, item.photo));
-      })
-      .catch(err => console.error(err));
-
-  }, []);
+  const { removeItem, incrementItem, decrementItem, getItemQty } = useCart();
 
   return (
     <div className="ci-cart-item">
       <div className="ci-item-image">
-        <img src={menuItem.photo} />
+        <img src={item.photo} />
       </div>
       <div className="ci-item-info">
-        <p className="ci-item-name">{menuItem.name}</p>
+        <p className="ci-item-name">{item.name}</p>
         <div className='ci-cart-ctrl-group'>
-          <button className='ci-cart-ctrl-btn' onClick={() => { incrementItem(menuItem.id) }}>+</button>
-          <span className='ci-cart-qty'>x{item.qty}</span>
-          <button className='ci-cart-ctrl-btn' onClick={() => { decrementItem(menuItem.id) }}>-</button>
+          <button className='ci-cart-ctrl-btn' onClick={() => { incrementItem(item.id) }}>+</button>
+          <span className='ci-cart-qty'>x{getItemQty(item.id)}</span>
+          <button className='ci-cart-ctrl-btn' onClick={() => { decrementItem(item.id) }}>-</button>
         </div>
       </div>
       <div className="ci-action-section">
-        <div className="ci-price"><p>${item.qty * menuItem.price}</p></div>
-        <button onClick={() => removeItem(menuItem.id)} className="ci-remove">X</button>
+        <div className="ci-price"><p>${getItemQty(item.id) * item.price}</p></div>
+        <button onClick={() => removeItem(item.id)} className="ci-remove">X</button>
       </div>
     </div>
   );
--- src/pages/Client/Cart/Cart.tsx
+++ src/pages/Client/Cart/Cart.tsx
@@ -2,31 +2,68 @@ import "./Cart.css";
 import { CartItem } from "../../../components/CartItem/CartItem";
 import { useNavigate } from "react-router-dom";
 import { useCart } from "../../../hooks/useCart";
+import { useEffect, useState } from "react";
+import axiosInstance from "../../../api/axios";
+import MenuItem from "../../../entities/MenuItem";
 
 export const Cart: React.FC = () => {
 
   const navigate = useNavigate();
   const { cart, clear } = useCart();
+  const [cartItems, setCartItems] = useState([] as MenuItem[]);
+  const [total, setTotal] = useState(0);
 
-  return (
-    <main className="cart-body">
-      <h1 className="cart-heading">Cart</h1>
-      <div className="cart-container">
-        {cart.map(item => <CartItem key={item.id} item={item} />)}
-      </div>
-      <div className="cart-total">
-        <p className="total-label">Total</p>
-        <p className="total-value">$1000</p>
-      </div>
-      <div className="cart-controls">
-        <button onClick={() => { navigate('/checkout') }} id="proceed-btn">Proceed to Checkout</button>
-        <button id="continue-btn">Add more items</button>
-        <button onClick={() => {
-          clear();
-          navigate('/restaurants');
-        }} id="cancel-btn">Cancel Order</button>
-      </div>
-    </main >
-  )
+  useEffect(() => {
+    if (cart.length > 0) {
+      const query = `{
+        cartItems(idList: [${cart.map(item => item.id)}]){
+          id,
+          name,
+          price,
+          description,
+          photo
+        }
+      }
+      `
+      axiosInstance.post('/', { query })
+        .then(result => {
+          const items: MenuItem[] = result.data.cartItems;
+          setCartItems(items);
+        })
+        .catch(err => console.error(err.message));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (cart.length > 0 && cartItems.length > 0) {
+      const newTotal = cart.reduce((previous, current) => {
+        previous += cartItems.find(item => item.id === current.id)!.price * current.qty;
+        return previous;
+      }, 0)
+      setTotal(newTotal);
+    }
+  }, [cart, cartItems])
+
+
+return (
+  <main className="cart-body">
+    <h1 className="cart-heading">Cart</h1>
+    <div className="cart-container">
+      {cartItems.map(item => <CartItem key={item.id} item={item} />)}
+    </div>
+    <div className="cart-total">
+      <p className="total-label">Total</p>
+      <p className="total-value">${total}</p>
+    </div>
+    <div className="cart-controls">
+      <button onClick={() => { navigate('/checkout') }} id="proceed-btn">Proceed to Checkout</button>
+      <button id="continue-btn">Add more items</button>
+      <button onClick={() => {
+        clear();
+        navigate('/restaurants');
+      }} id="cancel-btn">Cancel Order</button>
+    </div>
+  </main >
+)
 }
 
